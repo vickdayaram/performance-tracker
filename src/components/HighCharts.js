7272527637d5becc74c97324f621b264f07a8085
github username <github email address>
2017-11-03 13:24:41 -0400

    working on chart options

--- src/components/HighCharts.js
+++ src/components/HighCharts.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import Highcharts from 'highcharts/highstock';
-import { HighchartsChart, withHighcharts, XAxis, YAxis, Title, Subtitle, Legend, stockChart, LineSeries, Labels } from 'react-jsx-highcharts';
 import { connect } from 'react-redux'
 import formatCurrency from 'format-currency'
+import { HighchartsChart,
+          Tooltip, Chart,
+          withHighcharts,
+          XAxis, YAxis,
+          Title, Subtitle,
+          Legend, stockChart,
+          LineSeries, Labels } from 'react-jsx-highcharts';
+
 
 
 export const options = { format: '%s%v', symbol: '$' }
@@ -22,24 +29,30 @@ const chartOptions = {
 
 class ChartComponent extends Component {
 
+  format(){
+    return `$${this.y} on ${this.x}`
+  }
+
 
   render(){
 
     return (
         <div className="app">
           <HighchartsChart >
+            <Chart height={600}/>
+            <Tooltip crosshairs={true} formatter={this.format}/>
+            <Title align="right"> Historical Performance </Title>
 
-            <Title> Historical Performance </Title>
+            <Subtitle align="right">Source: alphavantage.com </Subtitle>
 
-            <Subtitle>Source: alphavantage.com</Subtitle>
-
-            <Legend layout="vertical" align="right" verticalAlign="middle" />
+            <Legend layout="horizontal" align="top" verticalAlign="top" />
 
             <XAxis categories={this.props.chartData.labels} >
               <XAxis.Title>Time</XAxis.Title>
             </XAxis>
 
             <YAxis id="number" >
+              <YAxis.Title>Stock Price</YAxis.Title>
               <LineSeries id="Stock Price" name={this.props.stock.selected} data={this.props.chartData.data} />
             </YAxis>
           </HighchartsChart>
